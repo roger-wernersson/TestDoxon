e64ae46c5245b3ffff0443680e3c68f873e98c2f
Rasmus Schenström <rasmus.schenstrom@ericsson.com>
2018-06-19 11:27:39 +0200

    Fix CompilationUnitEditor

--- src/testdoxon/listeners/TDPartListener.java
+++ src/testdoxon/listeners/TDPartListener.java
@@ -1,5 +1,6 @@
 package testdoxon.listeners;
 
+import org.eclipse.jdt.internal.ui.javaeditor.CompilationUnitEditor;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.custom.CaretListener;
 import org.eclipse.swt.custom.StyledText;
@@ -11,31 +12,33 @@ import org.eclipse.ui.texteditor.AbstractTextEditor;
 
 import testdoxon.handlers.FileCrawlerHandler;
 
+@SuppressWarnings("restriction")
 public class TDPartListener implements IPartListener {
 	private CaretListener wordListener;
 	private FileCrawlerHandler fileCrawlerHandler;
-	
+
 	private TableViewer viewer;
 
-	
 	public TDPartListener(TableViewer viewer, FileCrawlerHandler fileCrawlerHandler) {
 		super();
 		this.viewer = viewer;
 		this.fileCrawlerHandler = fileCrawlerHandler;
 	}
-	
+
 	@Override
 	public void partActivated(IWorkbenchPart arg0) {
 		// TODO Auto-generated method stub
 		IEditorPart iep = arg0.getSite().getPage().getActiveEditor();
 		if (iep != null) {
-			AbstractTextEditor e = (AbstractTextEditor) iep;
-			Control adapter = e.getAdapter(Control.class);
+			if (iep instanceof CompilationUnitEditor) {
+				AbstractTextEditor e = (AbstractTextEditor) iep;
+				Control adapter = e.getAdapter(Control.class);
 
-			if (adapter instanceof StyledText) {
-				StyledText text = (StyledText) adapter;
-				this.wordListener = new UpdateOnCaretMovedListener(this.viewer, text, this.fileCrawlerHandler);
-				text.addCaretListener(this.wordListener);
+				if (adapter instanceof StyledText) {
+					StyledText text = (StyledText) adapter;
+					this.wordListener = new UpdateOnCaretMovedListener(this.viewer, text, this.fileCrawlerHandler);
+					text.addCaretListener(this.wordListener);
+				}
 			}
 		}
 	}
@@ -43,13 +46,13 @@ public class TDPartListener implements IPartListener {
 	@Override
 	public void partBroughtToTop(IWorkbenchPart arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void partClosed(IWorkbenchPart arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -58,12 +61,14 @@ public class TDPartListener implements IPartListener {
 		if (this.wordListener != null) {
 			IEditorPart iep = arg0.getSite().getPage().getActiveEditor();
 			if (iep != null) {
-				AbstractTextEditor e = (AbstractTextEditor) iep;
-				Control adapter = e.getAdapter(Control.class);
+				if (iep instanceof CompilationUnitEditor) {
+					AbstractTextEditor e = (AbstractTextEditor) iep;
+					Control adapter = e.getAdapter(Control.class);
 
-				if (adapter instanceof StyledText) {
-					StyledText text = (StyledText) adapter;
-					text.removeCaretListener(this.wordListener);
+					if (adapter instanceof StyledText) {
+						StyledText text = (StyledText) adapter;
+						text.removeCaretListener(this.wordListener);
+					}
 				}
 			}
 		}
@@ -72,7 +77,7 @@ public class TDPartListener implements IPartListener {
 	@Override
 	public void partOpened(IWorkbenchPart arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
