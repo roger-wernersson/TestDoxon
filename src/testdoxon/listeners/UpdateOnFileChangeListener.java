d7cfcc37779724acd303368217538db9121d5806
Rasmus Schenström <rasmus.schenstrom@ericsson.com>
2018-06-19 08:20:13 +0200

    Fix

---
+++ src/testdoxon/listeners/UpdateOnFileChangeListener.java
@@ -0,0 +1,53 @@
+package testdoxon.listeners;
+
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IPartListener;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.texteditor.AbstractTextEditor;
+
+public class UpdateOnFileChangeListener implements IPartListener {
+
+	@Override
+	public void partActivated(IWorkbenchPart arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void partBroughtToTop(IWorkbenchPart arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void partClosed(IWorkbenchPart arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void partDeactivated(IWorkbenchPart arg0) {
+//		// TODO Auto-generated method stub
+//		if (wordListener != null) {
+//			IEditorPart iep = arg0.getSite().getPage().getActiveEditor();
+//			if (iep != null) {
+//				AbstractTextEditor e = (AbstractTextEditor) iep;
+//				Control adapter = e.getAdapter(Control.class);
+//
+//				if (adapter instanceof StyledText) {
+//					StyledText text = (StyledText) adapter;
+//					text.removeCaretListener(wordListener);
+//				}
+//			}
+//		}
+	}
+
+	@Override
+	public void partOpened(IWorkbenchPart arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/testdoxon/listeners/UpdateOnSaveListener.java
@@ -0,0 +1,14 @@
+package testdoxon.listeners;
+
+import org.eclipse.core.resources.IResourceChangeEvent;
+import org.eclipse.core.resources.IResourceChangeListener;
+
+public class UpdateOnSaveListener implements IResourceChangeListener {
+
+	@Override
+	public void resourceChanged(IResourceChangeEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
