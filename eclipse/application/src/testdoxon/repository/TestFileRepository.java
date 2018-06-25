717ae4dda7a47ce998b7905a6bfe100275724aeb
Philip Tennevall <philip.tennevall@ericsson.com>
2018-06-25 11:10:12 +0200

    Tests for FileRepository

---
+++ eclipse/application/src/testdoxon/repository/TestFileRepository.java
@@ -0,0 +1,47 @@
+package testdoxon.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import testdoxon.exceptionHandler.TDException;
+
+class TestFileRepository {
+
+	@Test
+	public void testFetchMethodNamesNotNull() throws TDException {
+		
+		String path = System.getProperty("user.dir") + "\\src\\testdoxon\\util\\TestDoxonUtils.java";
+		FileRepository FR = new FileRepository();
+		
+		assertNotNull(FR.fetchMethodNames(path));
+	}
+	
+	@Test
+	public void testFetchMethodNamesTriggerNull() throws TDException
+	{
+		String path = System.getProperty("user.dir") + "\\src\\testdoxon\\util\\DoxonUtils.java";
+		FileRepository FR = new FileRepository();
+		
+		assertNull(FR.fetchMethodNames(path));
+	}
+	
+	@Test
+	public void testFindLineNumberOfMethod() throws TDException
+	{
+		String path = System.getProperty("user.dir") + "\\src\\testdoxon\\util\\TestDoxonUtils.java";
+		FileRepository FR = new FileRepository();
+		
+		assertEquals(34,FR.findLineNumberOfMethod(path, "FindTestFolderReturn"));
+	}
+	
+	@Test
+	public void testFindLineNumberOfMethodReturnZero() throws TDException
+	{
+		String path = System.getProperty("user.dir") + "\\src\\testdoxon\\util\\TestDoxonUtils.java";
+		FileRepository FR = new FileRepository();
+		
+		assertEquals(0,FR.findLineNumberOfMethod(path, "FindTestFolder"));
+	}
+
+}
