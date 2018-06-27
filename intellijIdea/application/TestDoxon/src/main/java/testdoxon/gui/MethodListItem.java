9d1aa5308677af1aabef667d153af131c0e8692c
Rasmus Schenström <rasmus.schenstrom@ericsson.com>
2018-06-27 16:05:17 +0200

    Pictures

--- intellijIdea/application/TestDoxon/src/main/java/testdoxon/gui/MethodListItem.java
+++ intellijIdea/application/TestDoxon/src/main/java/testdoxon/gui/MethodListItem.java
@@ -1,23 +1,24 @@
 package testdoxon.gui;
 
+import testdoxon.utils.TestDoxonPluginIcons;
+
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class MethodListItem extends DefaultListCellRenderer {
 
     @Override
     public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
-
         Font font = new Font("Dialog", Font.PLAIN, 12);
         JLabel label = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+        label.setIcon(TestDoxonPluginIcons.blueDot);
+        label.setBorder(new EmptyBorder(0, 15, 0, 0));
 
-        System.out.println("MethodListItem: " + value.toString());
+        //System.out.println("MethodListItem: " + value.toString());
 
-        //label.setIcon
-        //label.setHorizontalAlignment(JLabel.);
         label.setFont(font);
         return label;
-        //return super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
     }
 
 
--- intellijIdea/application/TestDoxon/src/main/java/testdoxon/views/TestDoxonToolWindowFactory.java
+++ intellijIdea/application/TestDoxon/src/main/java/testdoxon/views/TestDoxonToolWindowFactory.java
@@ -2,21 +2,17 @@ package testdoxon.views;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.ComboBox;
-import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.components.JBLabel;
 import com.intellij.ui.components.JBList;
+import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentManager;
-import com.intellij.util.ui.GridBag;
-import groovy.swing.factory.LayoutFactory;
 import org.jetbrains.annotations.NotNull;
 import testdoxon.gui.MethodListItem;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
-import java.awt.font.TextAttribute;
-import java.util.Collections;
+
 
 public class TestDoxonToolWindowFactory implements com.intellij.openapi.wm.ToolWindowFactory {
     private JLabel header;
@@ -28,30 +24,7 @@ public class TestDoxonToolWindowFactory implements com.intellij.openapi.wm.ToolW
 
     public TestDoxonToolWindowFactory() {
         this.widgetColor = new Color(255, 255, 230);
-
-        this.content = new JPanel();
-        this.content.setLayout(new BorderLayout());
-        this.content.setBackground(this.widgetColor);
-
-        JPanel top = new JPanel();
-        top.setLayout(new GridLayout(2,1));
-        top.setBackground(this.widgetColor);
-
-        this.testClassesComboBox = new ComboBox();
-        top.add(this.testClassesComboBox);
-
-        this.header = new JBLabel("Open a class.");
-        this.header.setHorizontalAlignment(SwingConstants.CENTER);
-        this.header.setBounds(0,0, 100, 20);
-        this.header.setFont(new Font("Dialog", Font.BOLD, 12));
-        top.add(this.header);
-
-        this.testMethodList = new JBList();
-        this.testMethodList.setBackground(this.widgetColor);
-
-        content.add(top, BorderLayout.PAGE_START);
-        content.add(this.testMethodList, BorderLayout.CENTER);
-
+        this.initializeWidgets();
         this.addDummyData();
 
     }
@@ -70,6 +43,38 @@ public class TestDoxonToolWindowFactory implements com.intellij.openapi.wm.ToolW
 
         this.testMethodList.setCellRenderer(new MethodListItem());
         this.testMethodList.setListData(new String[] {"one", "two", "three"});
+    }
+
+    private void initializeWidgets() {
+        this.content = new JPanel();
+        this.content.setLayout(new BorderLayout());
+        this.content.setBackground(this.widgetColor);
+
+        JPanel top = new JPanel();
+        top.setLayout(new GridLayout(2,1));
+        top.setBackground(this.widgetColor);
+
+        this.testClassesComboBox = new ComboBox();
+        top.add(this.testClassesComboBox);
+
+        this.header = new JBLabel("Open a class.");
+        this.header.setHorizontalAlignment(SwingConstants.CENTER);
+        this.header.setBounds(0,0, 100, 20);
+        this.header.setFont(new Font("Dialog", Font.BOLD, 12));
+        top.add(this.header);
+
+
+        this.testMethodList = new JBList();
+        this.testMethodList.setBackground(this.widgetColor);
+
+        JBScrollPane scrollPane = new JBScrollPane();
+        scrollPane.createVerticalScrollBar();
+        scrollPane.createVerticalScrollBar();
+        scrollPane.setBorder(null);
+        scrollPane.setViewportView(this.testMethodList);
+
+        content.add(top, BorderLayout.PAGE_START);
+        content.add(scrollPane, BorderLayout.CENTER);
     }
 
 }
