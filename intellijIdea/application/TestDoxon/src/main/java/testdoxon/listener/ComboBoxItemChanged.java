e8f0d34e3fbc6e9da6f601cf9a243311cf3ee4d7
Rasmus Schenström <rasmus.schenstrom@ericsson.com>
2018-06-28 14:10:25 +0200

    Click on combobox functionallity

--- intellijIdea/application/TestDoxon/src/main/java/testdoxon/listener/ComboBoxItemChanged.java
+++ intellijIdea/application/TestDoxon/src/main/java/testdoxon/listener/ComboBoxItemChanged.java
@@ -1,11 +1,24 @@
 package testdoxon.listener;
 
+import com.intellij.ui.components.JBList;
+import testdoxon.model.TDFile;
 import testdoxon.model.TestFile;
+import testdoxon.utils.DoxonUtils;
+import testdoxon.utils.TDStatics;
 
+import javax.swing.*;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.io.File;
 
 public class ComboBoxItemChanged implements ItemListener {
+    private JLabel header;
+    private JBList testMethodList;
+
+    public ComboBoxItemChanged (JBList testMethodList, JLabel header) {
+        this.testMethodList = testMethodList;
+        this.header = header;
+    }
 
     @Override
     public void itemStateChanged(ItemEvent e) {
@@ -13,9 +26,10 @@ public class ComboBoxItemChanged implements ItemListener {
         if(e.getStateChange() == 1) {
             if(item instanceof TestFile) {
                 TestFile selected = (TestFile) item;
-                System.out.println(selected.getFilepath());
 
                 // Update JList with method from this test class
+                TDStatics.currentTestFile = new TDFile(new File(selected.getFilepath()));
+                DoxonUtils.setListItems(this.testMethodList, this.header);
             }
         }
     }
