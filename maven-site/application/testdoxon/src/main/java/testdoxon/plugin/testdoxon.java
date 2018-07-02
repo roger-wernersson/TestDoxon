81b5489944f5d8da75b74f176482b2ffb9ea9c31
Rasmus Schenström <rasmus.schenstrom@ericsson.com>
2018-07-02 13:01:33 +0200

    Maven-site

---
+++ maven-site/application/testdoxon/src/main/java/testdoxon/plugin/testdoxon.java
@@ -0,0 +1,15 @@
+package testdoxon.plugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "testdoxon")
+public class testdoxon extends AbstractMojo {
+
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		getLog().info("Hello testdoxon World");		
+	}
+
+}
